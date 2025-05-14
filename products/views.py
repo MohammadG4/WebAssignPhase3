45a53f7f26dd12037a3f3c9b528268ea3af1c2b1
Ahmed Mustafa <am1804067@gmail.com>
2025-05-14 16:42:01 +0300

    Search

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import recipes
+from django.db.models import Q
 
 def recipe_list(request):
     return render(request,'pages/recipe_list.html',{'key':recipes.objects.all()})
@@ -14,4 +15,29 @@ def Addrecipe(request):
     if(recipename and coursename and ingredients and description):
             alldata.save()
     return render(request,'pages/AddRecipe.html')
+
+
+
+def search_recipes(request):
+    query = request.GET.get('q', '')
+    ingredients = request.GET.get('ingredients', '')
+    
+    recipes_filtered = recipes.objects.all()
+    
+    if query:
+        recipes_filtered = recipes_filtered.filter(
+            Q(name__icontains=query) | Q(descripition__icontains=query)
+        )
+    
+    if ingredients:
+        ingredient_list = [i.strip() for i in ingredients.split(',')]
+        for ing in ingredient_list:
+            recipes_filtered = recipes_filtered.filter(ingredients__icontains=ing)
+
+    return render(request, 'pages/Search.html', {
+        'recipes': recipes_filtered,
+        'query': query,
+        'ingredients': ingredients,
+    })
+
 # Create your views here.
